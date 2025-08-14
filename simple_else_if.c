e0bcedf74e203adc5c28ef23496b433ee077ead4
Krishn-2822 <krishnupadhyay2822@gmail.com>
2025-08-14 14:59:22 +0530

    Add files via upload

---
+++ simple_else_if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter your Marks : ");
+    scanf("%d",&a);
+
+    if(a>90)
+    {
+        printf("Your Grade is : A+");
+    }else
+    {
+    printf("General walo ko admission nhi milta");
+    }
+    return 0;
+}
